a1c682e11d1eaf12fd8e60bd2b047415af4c3249
fatihdemir <fath.demmr@gmail.com>
2024-11-20 20:56:59 +0300

    Initial commit

---
+++ src/test/java/com/fatihdemir/websocketproject/WebSocketProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatihdemir.websocketproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
